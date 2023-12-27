7cacf1d542b98d7d078663878bf3d1f8c1ef9a05
June24-01 <huyquangtr01@gmail.com>
2023-12-28 00:25:07 +0700

    graft api + combine data

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,14 +6,50 @@ import Chart from 'chart.js/auto';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { IoIosOpen } from "react-icons/io";
+import useIncome from '../Income/useIncome';
+import useSpending from '../Spending/useSpending';
+// import useSpending from "../Dashboard/useSpending";
 
 function Index({ setTab }) {
   const chartRef = useRef(null);
   const chartDoughnutRef = useRef(null);
   const [value, onChange] = useState(new Date());
-
+  const [currentMonth, setCurrentMonth] = useState(null); // Thêm state cho tháng hiện tại
+  const { listIncomes } = useIncome()
+  const { listIncomes: listSpending } = useSpending()
+  const [newArr, setNewArr] = useState([])
+  const [total, setTotal] = useState(0)
+  
+  console.log(listIncomes, listSpending);
   useEffect(() => {
     (async function () {
+      // setNewArr([...newArr,  listIncomes])
+      // Tạo một mảng với 12 tháng ban đầu
+      let months = Array.from({ length: 12 }, (_, index) => ({
+        month: index + 1,
+        count: 0,
+        income: 0
+      }));
+
+      // Lặp qua từng đối tượng trong mảng dữ liệu
+      for (const item of listSpending) {
+        // Trích xuất tháng từ ngày trong đối tượng
+        const month = new Date(item.date).getMonth() + 1;
+
+        // Cập nhật tổng giá trị cho tháng tương ứng
+        months[month - 1].count += item.price;
+      }
+
+      for (const item of listIncomes) {
+        // Trích xuất tháng từ ngày trong đối tượng
+        const month = new Date(item.date).getMonth() + 1;
+
+        // Cập nhật tổng giá trị cho tháng tương ứng
+        months[month - 1].income += item.price;
+      }
+
+      console.log(months);
+
       const data = [
         { year: 2010, count: 30, income: 15 },
         { year: 2011, count: 40, income: 20 },
@@ -31,18 +67,18 @@ function Index({ setTab }) {
       chartRef.current = new Chart(document.getElementById('acquisitions'), {
         type: 'line',
         data: {
-          labels: data.map(row => row.year),
+          labels: months.map(row => row.month),
           datasets: [
             {
               label: 'Thu nhập',
-              data: data.map(row => row.count),
+              data: months.map(row => row.income),
               fill: false, // To display an unfilled line
               borderColor: 'green', // Line color
               tension: 0.1, // Line tension (0 for straight lines)
             },
             {
               label: 'Chi tiêu',
-              data: data.map(row => row.income),
+              data: months.map(row => row.count),
               fill: false, // To display an unfilled line
               borderColor: 'red', // Line color
               tension: 0.1, // Line tension (0 for straight lines)
@@ -51,11 +87,15 @@ function Index({ setTab }) {
         },
       });
 
+      // Tính tổng giá trị của tất cả các tháng
+      const totalOfAllMonthsIncome = months.reduce((sum, month) => sum + month.income, 0);
+      const totalOfAllMonthsSpending = months.reduce((sum, month) => sum + month.count, 0);
+      setTotal(totalOfAllMonthsIncome - totalOfAllMonthsSpending)
       const dataDoughnut = {
         labels: ['Thu', 'Chi'],
         datasets: [{
           label: 'My First Dataset',
-          data: [75, 25],
+          data: [totalOfAllMonthsIncome, totalOfAllMonthsSpending],
           backgroundColor: ['green', 'red'],
           hoverOffset: 4,
         }],
@@ -71,7 +111,8 @@ function Index({ setTab }) {
       });
 
     })();
-  }, []);
+  }, [listSpending, listIncomes]);
+
   return (
     <Layout tab={'dashboard'} setTab={setTab}>
       <div className='text-center text-3xl font-bold mt-10'>
@@ -91,28 +132,33 @@ function Index({ setTab }) {
                 <div>Thu - Chi</div>
                 <div size={50}><IoIosOpen /></div>
               </div>
-              <div className="flex justify-between justify-between items-center">
-                <div className="text-left">
-                  <div className="text-xl font-semibold text-left">Spotify</div>
-                  <div className="text-gray-400">4h 15/04/2023</div>
-                </div>
-                <div className="text-xl text-red-700 font-semibold">35.000VND</div>
+              <div className='h-[30vh] overflow-y-auto'>
+                {listSpending?.map(item => {
+                  return (
+                    <div className="flex justify-between justify-between items-center">
+                      <div className="text-left">
+                        <div className="text-xl font-semibold text-left">{item.name}</div>
+                        <div className="text-gray-400">{item.date}</div>
+                      </div>
+                      <div className="text-xl text-red-700 font-semibold">{item.price}</div>
+                    </div>
+                  )
+                })}
+
+                {listIncomes?.map(item => {
+                  return (
+                    <div className="flex justify-between justify-between items-center">
+                      <div className="text-left">
+                        <div className="text-xl font-semibold text-left">{item.name}</div>
+                        <div className="text-gray-400">{item.date}</div>
+                      </div>
+                      <div className="text-xl text-green-700 font-semibold">{item.price}</div>
+                    </div>
+                  )
+                })}
+
               </div>
-              <div className="flex justify-between justify-between items-center">
-                <div className="text-left">
-                  <div className="text-xl font-semibold text-left">Mua sữa tắm</div>
-                  <div className="text-gray-400">5h 16/04/2023</div>
-                </div>
-                <div className="text-xl text-red-700 font-semibold">200.000VND</div>
-              </div>
-              <div className="flex justify-between justify-between items-center">
-                <div className="text-left">
-                  <div className="text-xl font-semibold text-left">Bố Cho tiền</div>
-                  <div className="text-gray-400">22h 18/04/2023</div>
-                </div>
-                <div className="text-xl text-green-700 font-semibold">2.000.000VND</div>
-              </div>
-              <div className="flex justify-between justify-between items-center">
+              {/* <div className="flex justify-between justify-between items-center">
                 <div className="text-left">
                   <div className="text-xl font-semibold text-left">A trả tiền vay</div>
                   <div className="text-gray-400">10h 20/04/2023</div>
@@ -132,7 +178,7 @@ function Index({ setTab }) {
                   <div className="text-gray-400">12h 23/04/2023</div>
                 </div>
                 <div className="text-xl text-green-700 font-semibold">500.000VND</div>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="col-span-1 ml-5">
@@ -141,8 +187,7 @@ function Index({ setTab }) {
             </div>
             <div className="w-[400px] p-3 mt-3 border-[1px] px-10 border-gray-200 rounded-3xl text-left">
               <div className="text-gray-400">Tổng số tài sản hiện tại</div>
-              <div className="text-3xl font-semibold">2.500.000VND</div>
-              <div className="text-lime-500">+8.00%</div>
+              <div className="text-3xl font-semibold">{total} <sup>đ</sup></div>
             </div>
           </div>
         </div>
