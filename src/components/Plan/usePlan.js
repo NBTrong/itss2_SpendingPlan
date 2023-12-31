6ad62d07829ba8c2a48e068561ca75c1b814cfbf
June24-01 <huyquangtr01@gmail.com>
2024-01-01 02:46:21 +0700

    tooltip for spendingPlan

--- src/components/Plan/usePlan.js
+++ src/components/Plan/usePlan.js
@@ -1,114 +1,133 @@
-import { useCallback } from 'react';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import userKeyAtom from '../../storage/userKeyAtom';
-import {
-  useRecoilState
-} from 'recoil'
+import { useCallback } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import userKeyAtom from "../../storage/userKeyAtom";
+import { useRecoilState } from "recoil";
 
 import {
   getExpenses,
   addPlan,
   getCategories,
-  deleteExpense
-} from '../../services/api';
+  deleteExpense,
+} from "../../services/api";
 
 export default function usePlan() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-  const currentTime = (new Date()).toISOString();
+  const currentTime = new Date().toISOString();
 
-  const currentMonth = ((new Date()).getMonth() + 1).toString().padStart(2, '0')
+  const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, "0");
 
   const [userKey] = useRecoilState(userKeyAtom);
 
   // ----------------------------------- Get list incomes ----------------------------------------
   const listExpensesTransform = useCallback((data) => {
-    return data ? data.map((item) => ({
-      id: item?.id,
-      amount: item?.amount,
-      note: item?.note,
-      time: item?.time,
-      userKey: item?.user_key,
-      categoryId: item?.category_id,
-      createdAt: item?.created_at,
-      updatedAt: item?.updated_at,
-      category: {
-        id: item?.category?.id,
-        name: item?.category?.name,
-        color: item?.category?.color,
-        icon: item?.category?.icon,
-        status: item?.category?.status,
-        amount: item?.category?.amount
-      },
-    })) : []
+    return data
+      ? data.map((item) => ({
+          id: item?.id,
+          amount: item?.amount,
+          note: item?.note,
+          time: item?.time,
+          userKey: item?.user_key,
+          categoryId: item?.category_id,
+          createdAt: item?.created_at,
+          updatedAt: item?.updated_at,
+          category: {
+            id: item?.category?.id,
+            name: item?.category?.name,
+            color: item?.category?.color,
+            icon: item?.category?.icon,
+            status: item?.category?.status,
+            amount: item?.category?.amount,
+          },
+        }))
+      : [];
   }, []);
 
   const { data, isSuccess } = useQuery({
-    queryKey: ['spending', currentMonth],
-    queryFn: () => getExpenses({
-      userKey,
-      time: currentTime
-    }),
+    queryKey: ["spending", currentMonth],
+    queryFn: () =>
+      getExpenses({
+        userKey,
+        time: currentTime,
+      }),
     enabled: !!userKey,
-    select: (data) =>
-      listExpensesTransform(data.data.data),
+    select: (data) => listExpensesTransform(data.data.data),
     retry: 3,
-    staleTime: 5 * 1000
+    staleTime: 5 * 1000,
   });
 
   // --------------------------------------------- Get list category -------------------------------
   const categoryQuery = useQuery({
-    queryKey: ['category-spending'],
-    queryFn: () => getCategories({
-      status: 'expenses',
-      isPlan: true
-    }),
+    queryKey: ["category-spending"],
+    queryFn: () =>
+      getCategories({
+        status: "expenses",
+        isPlan: true,
+      }),
     enabled: !!userKey && !!isSuccess,
     select: (data) => listCategoryTransform(data.data.data, true),
     retry: 3,
-    staleTime: 5 * 1000
+    staleTime: 5 * 1000,
   });
 
   const allCategoryQuery = useQuery({
-    queryKey: ['category-all'],
-    queryFn: () => getCategories({
-      status: 'expenses',
-    }),
+    queryKey: ["category-all"],
+    queryFn: () =>
+      getCategories({
+        status: "expenses",
+      }),
     enabled: !!userKey && !!isSuccess,
     select: (data) => listCategoryTransform(data.data.data),
     retry: 3,
-    staleTime: 5 * 1000
+    staleTime: 5 * 1000,
   });
 
-  const listCategoryTransform = useCallback((categories, isPlan = false) => {
-    const result = categories ? categories.map(category => {
-      return {
-        id: category.id,
-        name: category.name,
-        color: category.color,
-        icon: category.icon,
-        status: category.status,
-        amount: category?.amount ? category?.amount : 0,
-        current: 0,
-        createdAt: category.created_at,
-        updatedAt: category.updated_at,
-      };
-    }) : [];
+  const listCategoryTransform = useCallback(
+    (categories, isPlan = false) => {
+      const result = categories
+        ? categories.map((category) => {
+            return {
+              id: category.id,
+              name: category.name,
+              color: category.color,
+              icon: category.icon,
+              status: category.status,
+              amount: category?.amount ? category?.amount : 0,
+              current: 0,
+              createdAt: category.created_at,
+              updatedAt: category.updated_at,
+              title: "",
+            };
+          })
+        : [];
 
-    if (isPlan) {
-      data.forEach(item => {
-        const categoryIndex = result.findIndex(category => {
-          return item.category.id === category.id
-        })
-        if (categoryIndex !== -1) {
-          result[categoryIndex].current += item.amount
-        }
-      })
-    }
-
-
-    return result
-  }, [data]);
+      if (isPlan) {
+        data.forEach((item) => {
+          const categoryIndex = result.findIndex((category) => {
+            return item.category.id === category.id;
+          });
+          if (categoryIndex !== -1) {
+            result[categoryIndex].current += item.amount;
+          }
+        });
+      }
+      result.forEach((item) => {
+        let numberOfDays =
+          new Date(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            0
+          ).getDate() - new Date().getDate();
+        let amountDay = (item.amount - item.current) / numberOfDays;
+        item.title =
+          "Mỗi ngày bạn sẽ cần tiêu " +
+          Math.round(amountDay) +
+          " để sử dụng hết số tiền";
+      });
+      return result;
+    },
+    [data]
+  );
 
   // ---------------------------------------------- Add plan ------------------------------------
   const addPlanMutation = useMutation({
@@ -116,7 +135,7 @@ export default function usePlan() {
       return addPlan(data);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['category-spending'] })
+      queryClient.invalidateQueries({ queryKey: ["category-spending"] });
     },
     onError: (error) => {
       console.log(error);
@@ -128,11 +147,11 @@ export default function usePlan() {
     mutationFn: (data) => {
       return deleteExpense({
         userKey: userKey,
-        id: data.id
+        id: data.id,
       });
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['spending'] })
+      queryClient.invalidateQueries({ queryKey: ["spending"] });
     },
     onError: (error) => {
       console.log(error);
